1045e645365c6ba189ddf0a8e840a8944406dcef
MD092-Hemil <hemil.kumbhani@multidots.com>
2019-04-08 17:48:40 +0530

    First Commit

--- flutter_bloc/lib/src/blocs/movies_bloc.dart
+++ flutter_bloc/lib/src/blocs/movies_bloc.dart
@@ -2,24 +2,23 @@ import 'package:flutter_bloc/src/model/popular_movies_model.dart';
 import 'package:flutter_bloc/src/resources/repository.dart';
 import 'package:rxdart/rxdart.dart';
 
-class MoviesBloc{
-
-  final _repository=Repository();
+class MoviesBloc {
+  final _repository = Repository();
   final _movieFetcher = PublishSubject<PopularMoviesModel>();
 
   //Creating Observable
   Observable<PopularMoviesModel> get allMovies => _movieFetcher.stream;
 
-  fetchPopularMovies() async{
-    PopularMoviesModel  popularMoviesModel=await _repository.getPopularMovies();
+  fetchPopularMovies() async {
+    PopularMoviesModel popularMoviesModel =
+        await _repository.getPopularMovies();
     _movieFetcher.sink.add(popularMoviesModel);
-
   }
 
   //Disposer to dispose stream object
-  dispose(){
+  dispose() {
     _movieFetcher.close();
   }
-
 }
- final bloc= MoviesBloc();
+
+final bloc = MoviesBloc();
